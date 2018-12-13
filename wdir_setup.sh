2f727807b1758c159f38c731f4d93789aa308550
Domenico Simone <dome.simone@gmail.com>
2018-12-13 14:59:55 +0100

    wdir_setup.sh script: added MATCH configuration

--- wdir_setup.sh
+++ wdir_setup.sh
@@ -7,6 +7,7 @@ else
     wdir=$1
 fi
 
+# MODEL
 mkdir -p ${wdir}/reference_transcripts
 
 ln -sf $(pwd -P)/quantizyme_model.3.snakefile ${wdir}
@@ -15,5 +16,23 @@ cp analysis.tab ${wdir}
 
 echo "pipeline_dir: \"`pwd`\"" | cat config.yaml - > ${wdir}/config.yaml
 
+# MATCH
+mkdir -p ${wdir}/match
+mkdir -p ${wdir}/read_datasets
+
+ln -sf $(pwd -P)/quantizyme_match.snakefile ${wdir}
+cp analysis.match.tab ${wdir}
+
+echo "pipeline_dir: \"`pwd`\"" | cat config.match.yaml - > ${wdir}/config.match.yaml
+
 echo "${wdir} created with this structure:"
 tree ${wdir}
+
+echo ""
+echo "### MODEL workflow"
+echo "Please put your input reference transcripts in the `reference_transcripts` dir."
+echo "Distribution of lengths of reference transcripts will be located in the `reference_transcripts_length_distribution` folder."
+echo ""
+echo "### MATCH workflow"
+echo "Please put your read datasets in fasta format in the `read_datasets` dir."
+echo "Results of the MATCH workflow will be located in the `match` folder."
